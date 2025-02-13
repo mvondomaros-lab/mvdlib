298ffe9039d37e83d8de4b301ed171c95752c212
Michael von Domaros <mvondomaros@uni-marburg.de>
2025-02-13 16:07:39 +0100

    Cleanup.

--- mvdlib/plots/__init__.py
+++ mvdlib/plots/__init__.py
@@ -1,5 +1,5 @@
 from mvdlib.plots.figsize import FigSize
-from mvdlib.plots.matplotlib import contourplot, kdeplot, _register_colormaps
+from mvdlib.plots.matplotlib import _register_colormaps, contourplot, kdeplot
 
 __all__ = [FigSize, contourplot, kdeplot]
 
