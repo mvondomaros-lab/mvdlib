ad2a537e335b61e2eb1684fbb95c023dfc12cd99
Michael von Domaros <mvondomaros@uni-marburg.de>
2025-02-13 16:07:19 +0100

    Laplace transform moved to helpers.

--- mvdlib/helpers.py
+++ mvdlib/helpers.py
@@ -53,3 +53,49 @@ def unwrap(x: NDArray[np.float64], box: float) -> None:
     for i in range(1, x.size):
         dx = x[i] - x[i - 1]
         x[i] -= np.round(dx / box) * box
+
+
+@numba.jit(nopython=True, fastmath=True)
+def _laplace_core(
+    x: NDArray[np.float64], t: NDArray, s: NDArray[np.float64]
+) -> NDArray[np.float64]:
+    """
+    Compute the numerical Laplace transform of a signal using a Riemann sum.
+
+    :param x: Input signal.
+    :param t: Time values.
+    :param s: Laplace frequency.
+
+    :return: The numerical Laplace transform.
+    """
+    lpl = np.empty_like(s)
+    for i in numba.prange(s.size):
+        lpl[i] = np.trapezoid(x * np.exp(-s[i] * t), t)
+    return lpl
+
+
+def laplace(
+    x: NDArray[np.float64], t: NDArray, s: NDArray[np.float64]
+) -> NDArray[np.float64]:
+    """
+    Compute the numerical Laplace transform of a signal using a Riemann sum.
+
+    :param x: Input signal.
+    :param t: Time values.
+    :param s: Laplace frequency.
+
+    :return: The numerical Laplace transform.
+    """
+    if x.ndim != 1:
+        raise ValueError("Input signal must be 1-dimensional.")
+    if t.ndim != 1:
+        raise ValueError("Time values must be 1-dimensional.")
+    if s.ndim != 1:
+        raise ValueError("Laplace frequencies must be 1-dimensional.")
+    if x.size < 2:
+        raise ValueError("Input signal must have at least 2 points.")
+    if x.size != t.size:
+        raise ValueError("Input signal and time values must have the same size.")
+    if not np.all(np.isclose(np.diff(t), t[1] - t[0])):
+        raise ValueError("Time values must be evenly spaced.")
+    return _laplace_core(x, t, s)
